8b6850b370ecbb254ee6a3b2f2940211a5852531
carlos <carlos@localhost.localdomain>
2021-03-02 22:34:59 +0100

    mre and more

--- ksql/cur_ksql.sh
+++ ksql/cur_ksql.sh
@@ -1,8 +1,18 @@
-tr '\n' ' ' < src/statements.sql | \
+#!/bin/bash
+file=$1
+host_ksql=$2
+echo -e "\n\n⏳ Waiting for ksqlDB to be available before launching CLI\n"
+while [ $(curl -s -o /dev/null -w %{http_code} http://'$host_ksql'8088/) -eq 000 ]
+do
+  echo -e $(date) "ksqlDB Server HTTP state: " $(curl -s -o /dev/null -w %{http_code} http://'$host_ksql':8088/) " (waiting for 200)"
+  sleep 5
+done
+
+tr '\n' ' ' < $file | \
 sed 's/;/;\'$'\n''/g' | \
 while read stmt; do
     echo '{"ksql":"'$stmt'", "streamsProperties": {}}' | \
-        curl -s -X "POST" "http://localhost:8088/ksql" \
+        curl -s -X "POST" "http://'$host_ksql':8088/ksql" \
              -H "Content-Type: application/vnd.ksql.v1+json; charset=utf-8" \
              -d @- | \
         jq
